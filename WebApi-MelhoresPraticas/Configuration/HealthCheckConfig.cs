341626c7c02686e46c07b3835a57eb9ee7b0ce52
Willian Ledier <willian.almeida@socialbank.com.br>
2022-09-13 17:18:18 -0300

    Add project files.

---
+++ WebApi-MelhoresPraticas/Configuration/HealthCheckConfig.cs
@@ -0,0 +1,46 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi_MelhoresPraticas.Extension;
+
+namespace WebApiCore.Swagger
+{
+    public static class HealthCheckConfig
+    {
+        public static void AddHealthCheckConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+              .AddCheck("HealthCheckCustomizado", new HealthCheckCustom())
+              .AddSqlServer(configuration.GetConnectionString("DefaultConnection"), name: "ConnectionStringSqlServer");
+
+            //services.AddHealthChecksUI();
+
+        }
+
+        public static void UseHealthCheckConfiguration(this IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/api/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapHealthChecksUI(options =>
+                {
+                    options.UIPath = "/api/hc-ui";
+                    options.ResourcesPath = "/api/hc-ui-resources";
+
+                    options.UseRelativeApiPath = false;
+                    options.UseRelativeResourcesPath = false;
+                    options.UseRelativeWebhookPath = false;
+                });
+
+            });
+
+        }
+    }
+}
---
+++ WebApi-MelhoresPraticas/Extension/HealthCheckCustom.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApi_MelhoresPraticas.Extension
+{
+    public class HealthCheckCustom : IHealthCheck
+    {
+       
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            try
+            {
+
+                if (true)
+                {
+                    return HealthCheckResult.Healthy();
+                }
+                else
+                {
+                    HealthCheckResult.Unhealthy();
+                }
+            }
+            catch (Exception)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
